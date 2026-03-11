b2cb8f0c42acfd1771dcda98de51a32bafdfcda2
Rachel Setiawan <akelsetiawan203@gmail.com>
2026-03-11 11:08:58 +0700

    11032025

--- produk/src/main/java/com/rachel/produk/ProdukApplication.java
+++ produk/src/main/java/com/rachel/produk/ProdukApplication.java
@@ -2,10 +2,12 @@ package com.rachel.produk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ProdukApplication {
 
 	public static void main(String[] args) {
